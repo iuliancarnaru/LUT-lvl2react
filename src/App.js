8d454f78bdfa9c20c52472c16ceb9e8e22783012
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-05-27 20:54:40 +0100

    palying with reusable components with render props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import './App.css';
-import Toggle from './Toggle';
+import React from "react";
+import "./App.css";
+import Toggle from "./ToggleRenderProps";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Toggle>
-          <h1>Toggle me</h1>
-        </Toggle>
+        <Toggle
+          display={({ on, toggle }) => (
+            <div>
+              {on && <h1>Show me</h1>}
+              <button onClick={toggle}>Show / Hide</button>
+            </div>
+          )}
+        />
+        <Toggle
+          display={({ on, toggle }) => (
+            <div>
+              {on && <h2>Item 1 | Item 2 | Item 3</h2>}
+              <button onClick={toggle}>Menu</button>
+            </div>
+          )}
+        />
       </header>
     </div>
   );
